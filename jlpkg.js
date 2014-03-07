e8d94b906e1a2af690078dd938c4bf4126ace87f
Iain Dunning <iaindunning@gmail.com>
2014-03-07 13:57:25 -0500

    Update pkg

--- jlpkg.js
+++ jlpkg.js
@@ -1,12 +1,9 @@
-var jlpkgApp = angular.module('jlpkgApp', []);
+var jlpkgApp = angular.module('jlpkgApp', ['ui.bootstrap']);
 
  
 jlpkgApp.controller('jlpkgCtrl', function ($scope, $http) {
 
-  $scope.test = 5;
-
   $scope.stable_url = "all.json";
-
   $http.get($scope.stable_url).then(function(response){
     $scope.stable = response.data;
     // for (pkg in $scope.stable) {
@@ -16,21 +13,45 @@ jlpkgApp.controller('jlpkgCtrl', function ($scope, $http) {
     for (var i = 0; i < $scope.stable.length; i++) {
       url_split = $scope.stable[i].url.split('/');
       $scope.stable[i].owner = url_split[url_split.length-2];
+      $scope.stable[i].showtestlog = false;
     }
   });
 
   $scope.humanStatus = function(status){
     if (status == "full_pass") {
-      return "Tests Pass";
+      return "Tests pass.";
     } else if (status == "full_fail") {
-      return "Tests Fail";
+      return "Tests fail.";
     } else if (status == "using_pass") {
-      return "Can Load";
+      return "No tests, package loads.";
     } else if (status == "using_fail") {
-      return "Can't Load";
+      return "No tests, package doesn't load.";
     } else {
-      return "Unknown";
+      return "Package was untestable due to binary dependency.";
     }
   };
 
+  // Filtering
+  $scope.jlVer = "0.2";
+  $scope.testShow = {"full_pass":true,"full_fail":true,
+                     "using_pass":true,"using_fail":false,
+                     "not_possible":true};
+  $scope.searchName = "";
+  $scope.searchAuthor = "";
+});
+
+
+jlpkgApp.filter('pkgFilter', function() {
+    return function(items, jlVer, testShow, searchName, searchAuthor) {
+      var filtered = [];
+      angular.forEach(items, function(item) {
+        if( jlVer == item.jlver && 
+            testShow[item.status] &&
+            item.name.indexOf(searchName) != -1 &&
+            item.owner.indexOf(searchAuthor) != -1) {
+          filtered.push(item);
+        }
+      });
+      return filtered;
+    };
 });
---
+++ pulljson.sh
@@ -0,0 +1,7 @@
+scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/stable/concat.json stable.json
+scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/nightly/concat.json nightly.json
+echo "[" > all.json
+cat stable.json >> all.json
+echo "," >> all.json
+cat nightly.json >> all.json
+echo "]" >> all.json
