5e047f1d6328a411de89544960f8dcd56b1f7e52
Iain Dunning <iaindunning@gmail.com>
2014-03-27 15:19:40 -0400

    Real bump

--- concat.jl
+++ concat.jl
@@ -2,16 +2,16 @@ stable = readall("stable.json")
 nightly = readall("nightly.json")
 output = open("all.json","w")
 println(output, "[")
-println(stable[end])
+#println(stable[end])
 #if stable[end]==","
-    println(output, stable)
+#    println(output, stable)
 #else
-#    println(output, stable, ",")
+    println(output, stable, ",")
 #end
 #if nightly[end]==","
-    println(output, nightly[1:end-1])
+#    println(output, nightly[1:end-1])
 #else
-#    println(output, nightly)
+    println(output, nightly)
 #end
 println(output, "]")
 close(output)
