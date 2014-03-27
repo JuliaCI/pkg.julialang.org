b657d56f552723df4a485f830ea6ee4ec5a6b297
Iain Dunning <iaindunning@gmail.com>
2014-03-27 13:18:55 -0400

    Update

--- pulljson.sh
+++ pulljson.sh
@@ -1,8 +1,4 @@
 scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/stable/concat.json stable.json
 scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/nightly/concat.json nightly.json
-echo "[" > all.json
-cat stable.json >> all.json
-echo "," >> all.json
-cat nightly.json >> all.json
-echo "]" >> all.json
+julia concat.jl
 julia add_desc.jl
