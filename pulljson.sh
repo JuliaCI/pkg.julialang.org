644ae923e47517bbcd691946084c1e0d582cbb1d
Iain Dunning <iaindunning@gmail.com>
2014-03-30 11:40:52 -0400

    Add history tracking

--- pulljson.sh
+++ pulljson.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/stable/concat.json stable.json
 scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/nightly/concat.json nightly.json
 julia concat.jl
 julia add_desc.jl
+julia add_hist.jl $1
