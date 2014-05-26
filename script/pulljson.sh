81f85e9a02350aeb0322b220eac78452ec94d492
Iain Dunning <iaindunning@gmail.com>
2014-05-26 09:56:43 -0400

    Bump for 20140526

--- script/pulljson.sh
+++ script/pulljson.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/stable/concat.json stable.json
+scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/stable/concat.json stable.json
 scp idunning@che.mit.edu:~/PackageEvaluator.jl/extra/nightly/concat.json nightly.json
 julia enhance_json.jl $1
 julia build_html.jl
