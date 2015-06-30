9be3fb0ab13451cfab6a0f71633432d774614e5a
Iain Dunning <iaindunning@gmail.com>
2015-06-30 10:24:53 -0400

    Bump

--- script/pulljson.sh
+++ script/pulljson.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-scp idunning@che.mit.edu:~/PackageEvaluator.jl/scripts/releaseAL.json releaseAL.json
-scp idunning@che.mit.edu:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
-scp idunning@che.mit.edu:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
-scp idunning@che.mit.edu:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
+scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseAL.json releaseAL.json
+scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
+scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
+scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
 julia enhance_json.jl $1
 echo "BUILD HTML"
 julia build_html.jl
