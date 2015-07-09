5e7466c6aaee1f7bc34bbb6905be61d9b717e659
Iain Dunning <iaindunning@gmail.com>
2015-07-09 10:16:10 -0400

    Bump

--- script/pulljson.sh
+++ script/pulljson.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseAL.json releaseAL.json
-scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
-scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
-scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
+#scp idunning@50.136.95.12:~/PackageEvaluator.jl/scripts/releaseAL.json releaseAL.json
+#scp idunning@50.136.95.12:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
+#scp idunning@50.136.95.12:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
+#scp idunning@50.136.95.12:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
+#scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseAL.json releaseAL.json
+#scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
+#scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
+#scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
 julia enhance_json.jl $1
 echo "BUILD HTML"
 julia build_html.jl
