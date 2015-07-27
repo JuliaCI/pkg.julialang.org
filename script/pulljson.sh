e7584cb4b3bd5d6598d6d3eaac8825f2a6bc13cb
Iain Dunning <iaindunning@gmail.com>
2015-07-26 22:59:53 -0400

    July 26th

--- script/pulljson.sh
+++ script/pulljson.sh
@@ -7,6 +7,11 @@
 #scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/releaseMZ.json releaseMZ.json
 #scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyAL.json nightlyAL.json
 #scp idunning@192.168.8.104:~/PackageEvaluator.jl/scripts/nightlyMZ.json nightlyMZ.json
+cp /Users/idunning/.julia/v0.3/PackageEvaluator/scripts/releaseAL.json releaseAL.json
+cp /Users/idunning/.julia/v0.3/PackageEvaluator/scripts/releaseMZ.json releaseMZ.json
+cp /Users/idunning/.julia/v0.3/PackageEvaluator/scripts/nightlyAL.json nightlyAL.json
+cp /Users/idunning/.julia/v0.3/PackageEvaluator/scripts/nightlyMZ.json nightlyMZ.json
+
 julia enhance_json.jl $1
 echo "BUILD HTML"
 julia build_html.jl
